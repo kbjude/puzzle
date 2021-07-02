3a1245b204d67ae70ee83258bedba92dea759f20
kbjude <kbjude@gmail.com>
2021-07-03 02:40:39 +0300

    WIP puzzle game

--- puzzle-html/puzzle.js
+++ puzzle-html/puzzle.js
@@ -46,7 +46,22 @@ const swapBoxes = (grid, box1, box2) => {
 
 const isSolved = grid => {
     return(
-        grid[0][0] == 1 && 
+        grid[0][0] === 1 && 
+        grid[0][1] === 2 && 
+        grid[0][2] === 3 &&
+        grid[0][3] === 4 &&  
+        grid[1][0] === 5 && 
+        grid[1][1] === 6 &&
+        grid[1][2] === 7 && 
+        grid[1][3] === 8 && 
+        grid[2][0] === 9 && 
+        grid[2][1] === 10 &&
+        grid[2][2] === 11 &&  
+        grid[2][3] === 12 && 
+        grid[3][0] === 13 && 
+        grid[3][1] === 14 && 
+        grid[3][2] === 15 && 
+        grid[3][2] === 0
     )
 }
 
@@ -83,5 +98,65 @@ class State {
 
     static start(){
         return new State(getRandomGrid(), 0, 0, "playing");
+    }
+}
+
+class Game {
+    constructor(state) {
+        this.state = state;
+        this.tickedId = null;
+        this.tick = this.tick.bind()
+    }
+
+    static ready() {
+        return new Game(state.ready)
+    }
+
+    tick() {
+        this.setState({ time: this.state.time + 1})
+    }
+
+    setState(newState) {
+        this.state = { ... this.state, ...newState};
+        this.render();
+    }
+
+    handleClickBox(box) {
+        return function {
+            const nextdoorBoxes = box.getNextdoorBoxes();
+            const blankBox = nextdoorBoxes.find(
+                nextdoorBoxes => this.state.grid[nextdoorBoxes.y][nextdoorBoxes.x] === 0
+            );
+
+            if (blankBox) {
+                const newGrid = [... this.state.grid];
+                swapBoxes(newGrid, box, blankBox);
+                if (isSolved(newGrid)) {
+                    clearInterval(this.tickId);
+                } else {
+
+                }
+            }
+        }
+    }
+
+    render() {
+        const { grid, move, time, status } = this.state
+
+        //Render grid
+
+        const newGrid = document.createElement("div");
+        newGrid.className = "grid";
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                const button = document.createElement("button");
+                if(status === "playing") {
+                    button.addEventListener("click", this.handleClickBox())
+                }
+
+                button.textContent = grid[i][j] === 0 ? "" : grid[i][j].toString();
+                newGrid.appendChild(button);
+            }
+        }
     }
 }
